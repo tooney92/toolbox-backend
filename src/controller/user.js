c229240dab5b8ee60d2977359767047b5da9e59b
anthugny <62931219+anthugny@users.noreply.github.com>
2021-04-26 13:55:57 +0100

    added user signUp and login controllers and route

---
+++ src/controller/user.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const User = require("../model/user")
+const jwt = require("jsonwebtoken")
+const logger = require('../../util/winston');
+const {uuid} = require('uuidv4');
+const { Validator } = require('node-input-validator')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const webtoken = require("../../middlewares/auth")
+const helper = require("../../helpers/errorFormater")
+const _ = require("lodash")
+
+
+module.exports.signUp = async(req, res)=>{
+    try {
+        const v = new Validator(req.body,{
+            fullName: "required",
+            userName: "required",
+            email: "required|email",
+            gender: "required",
+            password: "required",
+            phoneNumber: "required"
+        })
+
+        const match = await v.check()
+        if(!match) return res.status(422).json({error:  helper.vErrorsMessageFormatter(v.errors)})
+        req.body.password = bcrypt.hashSync(req.body.password, saltRounds)
+        let user = new User(req.body)
+        await user.save()
+
+        //password is being sent, FIX
+
+        user = _.omit(user.toObject(), "password")
+        //email
+        res.json({user})
+
+    } catch (error) {
+        logger.error(`route: /users/signup, message - ${error.message}, stack trace - ${error.stack}`);
+        if(error.code === 11000) return res.status(422).json({error: helper.duplicateMessageFormatter(error.keyPattern)})
+        res.status(500).send("unable to perform request")
+    }
+}
+
+//check user exists and account is not deactivated or deleted.
+module.exports.login = async(req, res)=>{
+    try {
+        const v = new Validator(req.body,{
+            email: "required|email",
+            password: "required",
+        })
+        const match = await v.check()
+        if(!match) return res.status(422).json({error:  helper.vErrorsMessageFormatter(v.errors)})
+        const loginFields = {
+            profile_picture: 1,
+            deactivated: 1,
+            deleted: 1,
+            fullName: 1,
+            userName: 1,
+            email: 1,
+            gender: 1,
+            password: 1,
+            phoneNumber: 1,
+            createdAt: 1
+        }
+        let userInfo = await User.findOne({email: req.body.email}, loginFields)
+        userInfo = userInfo.toObject()
+        const matchPassword = await bcrypt.compare(req.body.password, userInfo.password)
+        if(!matchPassword){
+            return res.status(401).json({error: "User Password is Incorrect"})
+        } 
+        if(userInfo.deactivated || userInfo.deleted){
+            return res.status(401).send({error: userInfo.deleted ? "account deleted, contact admin" : "account deactivated contact admin"})
+        }
+        userInfo = _.omit(userInfo, "password")
+        //jwt
+        await jwt.sign({data: userInfo}, process.env.token_secret, {expiresIn: 60 * 60 * 2}, (err, token) => {
+            if(err) return res.status(500).json({message: "Token Could not be generated. Please try logging in again!"})
+            return res.status(200).json({
+                userInfo,
+                token: token
+            })
+        })
+    } catch (error) {
+        logger.error(`route: /users/error, message - ${error.message}, stack trace - ${error.stack}`);
+        res.status(500).send("unable to perform request")
+    }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,51 +1,19 @@
 const router = require('express').Router();
-const User = require("../model/user")
-const jwt = require("jsonwebtoken")
-const logger = require('../../util/winston');
-const {uuid} = require('uuidv4');
-const { Validator } = require('node-input-validator')
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const webtoken = require("../../middlewares/auth")
-const helper = require("../../helpers/errorFormater")
-const _ = require("lodash")
+const {signUp, login} = require("../controller/user")
 
-router.post('/user/signup', async(req, res)=>{
-    try {
-        const v = new Validator(req.body,{
-            fullName: "required",
-            userName: "required",
-            email: "required|email",
-            gender: "required",
-            password: "required",
-            phoneNumber: "required"
-        })
+router.post('/user/signup', signUp) 
 
-        const match = await v.check()
-        if(!match) return res.status(422).json({error:  helper.vErrorsMessageFormatter(v.errors)})
-        req.body.password = bcrypt.hashSync(req.body.password, saltRounds)
-        let user = new User(req.body)
-        await user.save()
+router.post('/user/login', login)
 
-        user = _.omit(user, "password")
-        //email
-        res.json({user})
 
-    } catch (error) {
-        logger.error(`route: /users/signup, message - ${error.message}, stack trace - ${error.stack}`);
-        if(error.code === 11000) return res.status(422).json({error: helper.duplicateMessageFormatter(error.keyPattern)})
-        res.status(500).send("unable to perform request")
-    }
-})
-
-router.get('/user/error', async(req, res)=>{
-    try {
-        // res.send("hello")
-        throw new Error("exception occured")
-    } catch (error) {
-        logger.error(`route: /users/error, message - ${error.message}, stack trace - ${error.stack}`);
-        res.status(500).send(`Our server is busy right now. Please try later.`)
-    }
-})
+// router.get('/user/error', async(req, res)=>{
+//     try {
+//         // res.send("hello")
+//         throw new Error("exception occured")
+//     } catch (error) {
+//         logger.error(`route: /users/error, message - ${error.message}, stack trace - ${error.stack}`);
+//         res.status(500).send(`Our server is busy right now. Please try later.`)
+//     }
+// })
 
 module.exports = router;
