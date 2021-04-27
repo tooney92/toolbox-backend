91e550c18fb3a29341e6ac33cefa4a7f64334fbc
anthugny <62931219+anthugny@users.noreply.github.com>
2021-04-27 03:41:11 +0100

    Update serviceCategory.js

--- src/controller/serviceCategory.js
+++ src/controller/serviceCategory.js
@@ -6,75 +6,77 @@ const bcrypt = require('bcrypt');
 const helper = require("../../helpers/errorFormater")
 
 
-module.exports.create = async(req, res)=>{
+module.exports.create = async (req, res) => {
     try {
-        const v = new Validator(req.body,{
+        const v = new Validator(req.body, {
             title: "required|minLength:3",
         })
         const match = await v.check()
-        if(!match) return res.status(422).json({error:  helper.vErrorsMessageFormatter(v.errors)})
+        if (!match) return res.status(422).json({ error: helper.vErrorsMessageFormatter(v.errors) })
         req.body.created_by = req.user._id
         const newServiceCategory = new serviceCategory(req.body)
         let info = await newServiceCategory.save()
-        res.json({info})
+        res.json({ info })
     } catch (error) {
         logger.error(`route: /service-category/, message - ${error.message}, stack trace - ${error.stack}`);
-        if(error.code === 11000) return res.status(409).json({error: helper.duplicateMessageFormatter(error.keyPattern)})
+        if (error.code === 11000) return res.status(409).json({ error: helper.duplicateMessageFormatter(error.keyPattern) })
         res.status(500).send("unable to perform request")
     }
 }
 
-module.exports.getAll = async(req, res)=>{
+module.exports.getAll = async (req, res) => {
     try {
         let categories = await serviceCategory.find()
-        res.json({categories})
+        res.json({ categories })
     } catch (error) {
         logger.error(`route: /service-category/, message - ${error.message}, stack trace - ${error.stack}`);
         res.status(500).send("unable to perform request")
     }
 }
 
-module.exports.getOne = async(req, res)=>{
+module.exports.getOne = async (req, res) => {
     try {
-        let category = await serviceCategory.findOne({_id: req.params.id})
-        res.json({category})
+        let category = await serviceCategory.findOne({ _id: req.params.id })
+        res.json({ category })
     } catch (error) {
         logger.error(`route: /service-category/, message - ${error.message}, stack trace - ${error.stack}`);
         res.status(500).send("unable to perform request")
     }
 }
 
-module.exports.updateOne = async(req, res)=>{
+module.exports.updateOne = async (req, res) => {
     try {
-        const v = new Validator(req.body,{
+        const v = new Validator(req.body, {
             title: "required|minLength:3",
         })
         console.log(req.body);
         const match = await v.check()
-        if(!match) return res.status(422).json({error:  helper.vErrorsMessageFormatter(v.errors)})
+        if (!match) return res.status(422).json({ error: helper.vErrorsMessageFormatter(v.errors) })
         let updatedCategory = await serviceCategory.findOneAndUpdate({
             _id: req.params.id
-        },{
-            $set:req.body
-        })
-        if(!updatedCategory){
+        }, {
+            $set: req.body
+        },
+            { new: true }
+        )
+        if (!updatedCategory) {
             return res.status(422).send("unable to update. Check Id")
         }
-        return res.json({updatedCategory})
+        return res.json({ updatedCategory })
     } catch (error) {
         logger.error(`route: /service-category/, message - ${error.message}, stack trace - ${error.stack}`);
-        if(error.code === 11000) return res.status(409).json({error: helper.duplicateMessageFormatter(error.keyPattern)})
+        if (error.code === 11000) return res.status(409).json({ error: helper.duplicateMessageFormatter(error.keyPattern) })
         res.status(500).send("unable to perform request")
     }
 }
 
-module.exports.deleteOne = async(req, res)=>{
+module.exports.deleteOne = async (req, res) => {
     try {
-        
+
         let info = await serviceCategory.findOneAndDelete({
             _id: req.params.id
         })
-        if(!info){
+        if (!info) {
             return res.status(404).send("category does not exist")
         }
         res.send("delete sucessful")
